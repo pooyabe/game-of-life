2290141996f4c6efb87bb8a20292276cb4b89329
pooyabe <pooyabehravesh@gmail.com>
2023-05-17 01:51:10 +0330

    Everything works. my world works :))))

--- gameoflife.c
+++ gameoflife.c
@@ -26,24 +26,24 @@ void give_life(void *pworld, int w, int h)
     char(*world)[w] = pworld,
     new_world[w][h]; // New world for storing the analizes on the current world;
 
-    int lives; // for counting the number of living neighbors
+    // int lives; // for counting the number of living neighbors
 
     // Browse all items and one item arround
     for (int x = 0; x < w; x++)
         for (int y = 0; y < h; y++)
         {
-            lives = 0;
+            int lives = 0;
             // xa means the x around this element. and ya so on
             for (int xa = x - 1; xa <= x + 1; xa++)
-                for (int ya = y - 1; ya < y + 1; ya++)
+                for (int ya = y - 1; ya <= y + 1; ya++)
                     if (world[(ya + h) % h][(xa + w) % w])
                         lives++;
             
-            if (world[x][y])
+            if (world[y][x])
                 lives--;
 
             // Create the new world at this element
-            new_world[x][y] = (lives == 3 || (lives == 2 && world[x][y]));
+            new_world[y][x] = (lives == 3 || (lives == 2 && world[y][x]));
         }
 
     // Seed the main world based on the new world
@@ -58,13 +58,20 @@ int main()
     int width = 30, height = width;
     char world[width][height];
 
+    // Get the time for sleep
+    int sleep_time;
+    printf("Enter a sleep time between 1 to 10 (1 is fastest. 100 slowest) :");
+    scanf("%d", &sleep_time);
+
+    sleep_time *= 100000;
+
     // Seed the random generator with time
     srand(time(NULL));
 
     // Seed the world
     for (int x = 0; x < width; x++)
         for (int y = 0; y < height; y++)
-            world[x][y] = rand() % 11 < 5 ? 1 : 0; // Randomly seed the world homes
+            world[x][y] = rand() % 11 < 2 ? 1 : 0; // Randomly seed the world homes
 
     while (1)
     {
@@ -74,7 +81,7 @@ int main()
         give_life(world, width, height);
 
         // get some rest in 6th day. or maybe 7 i dont now much about it!
-        usleep(100000);
+        usleep(sleep_time);
     }
 
     // Some additional line breaks
