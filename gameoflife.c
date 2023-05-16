8ccebbcef91aa8baaa93c25e7b05292ef47d196b
pooyabe <pooyabehravesh@gmail.com>
2023-05-17 00:52:39 +0330

    Draw the first board and fixed some errors

--- gameoflife.c
+++ gameoflife.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void draw(void *pworld, int w, int h)
+{
+    char(*board)[w] = pworld;
+
+    for (int x = 0; x < w; x++)
+    {
+        for (int y = 0; y < h; y++)
+            printf("%c", board[x][y]);
+        printf("\n");
+    }
+}
+
 int main()
 {
     // Declear the variables
@@ -9,8 +21,13 @@ int main()
     // Seed the world
     for (int x = 0; x < width; x++)
         for (int y = 0; y < height; y++)
-            world[x][y] = 1;
-    
+            world[x][y] = '+';
 
+    // Draw the first world
+    draw(world, width, height);
+
+
+    // Some additional line breaks
+    printf("\n\n\n");
     return 0;
 }
